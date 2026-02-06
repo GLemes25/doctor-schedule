5e9a77220836efea4bddfa0b3fe86d031f045a27
GLemes25 <gabriellemes924@gmail.com>
2026-02-06 10:53:08 -0400

    chore: add prettier and eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -19,7 +21,29 @@ const eslintConfig = defineConfig([
       "simple-import-sort": simpleImportSort,
     },
     rules: {
-      "simple-import-sort/imports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // 1️⃣ Side-effect imports → SEMPRE no topo (CSS, polyfills, etc)
+            ["^\\u0000"],
+
+            // 2️⃣ Next.js / React
+            ["^next", "^react"],
+
+            // 3️⃣ Packages
+            ["^@?\\w"],
+
+            // 4️⃣ Absolute imports (ex: @/)
+            ["^@/"],
+
+            // 5️⃣ Relative imports
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ],
+        },
+      ],
+
       "simple-import-sort/exports": "error",
     },
   },
